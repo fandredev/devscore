ecbf801f87f257525897b3f393d17b9a61f3eb1f
Felipe <profissionalf.andre@gmail.com>
2020-11-07 00:54:44 -0300

    feat: adding first files, styles, images and more

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { Title } from '../styles/Landing'
+
+export default function Home() {
+  return <Title>My page</Title>
+}
