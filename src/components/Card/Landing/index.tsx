4bcbd9b95605e072200604b609af95c6ce7d8c6c
Felipe <profissionalf.andre@gmail.com>
2020-11-14 19:38:25 -0300

    test: verified build in vercel

--- src/components/Card/Landing/index.tsx
+++ src/components/Card/Landing/index.tsx
@@ -1,7 +1,7 @@
 import { Aside } from './styled'
 import { CardProps } from '../../../interfaces'
 
-export default function Card({ url, alt, text } : CardProps<string>) : JSX.Element {
+export default function Card({ alt, text } : CardProps<string>) : JSX.Element {
   return (
       <Aside>
         <img src="../../../../public/images/icons/java.svg" alt={alt} />
